988263b2dea8d767b03ddfc7dc9c7d2f5cb393e6
Bismark Agyare <bismarkagyare54@gmail.com>
2024-10-24 09:51:13 +0000

    redo problem

--- phoneNumber.js
+++ phoneNumber.js
@@ -16,14 +16,15 @@
 // };
 
 const createPhoneNumber = (numbers) => {
-  let format = "(xxx) xxx-xxxx";
+  let format = "(xxx) xxx-xxxx"
 
-  for (let i = 0; i < numbers.length; i++) {
-    format = format.replace('x', numbers[i])  // "(1xx) xxx-xxxx": after first iteration
+  for (i = 0; i < numbers.length; i++) {
+    format = format.replace('x', numbers[i])
   }
 
   return format;
 }
+
 
 const res = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
 console.log("result: ", res);
