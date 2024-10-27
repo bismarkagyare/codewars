4e537c61ff2efc5c36a0115463590cccd21fa796
Bismark Agyare <bismarkagyare54@gmail.com>
2024-10-27 13:34:14 +0000

    optimised and concise solution

--- shortestWord.js
+++ shortestWord.js
@@ -1,20 +1,22 @@
 // Simple, given a string of words, return the length of the shortest word(s).
 
+// const findShort = (s) => {
+//   const wordsArr = s.split(' ')
+//   let wordInitialiser = 100000
+
+//   for (i = 0; i < wordsArr.length; i++) {
+//     let wordLength = wordsArr[i].length;
+//     if (wordLength < wordInitialiser) {
+//       wordInitialiser = wordLength
+//     }
+//   }
+
+//   return wordInitialiser;
+// }
+
 const findShort = (s) => {
-  const wordsArr = s.split(' ')
-  let wordInitialiser = 100000
-
-  for (i = 0; i < wordsArr.length; i++) {
-    let wordLength = wordsArr[i].length;
-    if (wordLength < wordInitialiser) {
-      wordInitialiser = wordLength
-    }
-  }
-
-  return wordInitialiser;
-}   
-
-
+  return Math.min(...s.split(" ").map((word) => word.length));
+};
 
 const res = findShort("might santiago trafford");
-console.log(res)
+console.log(res);
