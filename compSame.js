dae2e382cf1ba44391c267850d58179dbe44f8ea
Bismark Agyare <bismarkagyare54@gmail.com>
2024-10-29 14:12:27 +0000

    optimise solution

--- compSame.js
+++ compSame.js
@@ -1,21 +1,33 @@
+// const compSame = (a, b) => {
+//   if (a.length !== b.length) return false;
+
+//   if (!a.length || !b.length) return false;
+
+//   const squaresOfA = a.map((a) => a * a);
+
+//   const sortedA = squaresOfA.sort((a, b) => a - b);
+//   const sortedB = b.sort((a, b) => a - b);
+
+//   for (i = 0; i < sortedA.length; i++) {
+//     if (sortedA[i] !== sortedB[i]) {
+//       return false
+//     }
+//   }
+
+//   return true
+
+// };
+
 const compSame = (a, b) => {
   if (a.length !== b.length) return false;
 
   if (!a.length || !b.length) return false;
 
-  const squaresOfA = a.map((a) => a * a);
+  const sortedA = a.map(num => num * num).sort((x, y) => x - y)
+  const sortedB = b.sort((x, y) => x - y)
 
-  const sortedA = squaresOfA.sort((a, b) => a - b);
-  const sortedB = b.sort((a, b) => a - b);
+  return sortedA.every((value, index) => value === sortedB[index])
+}
 
-  for (i = 0; i < sortedA.length; i++) {
-    if (sortedA[i] !== sortedB[i]) {
-      return false
-    }
-  }
-
-  return true
-};
-
-const res = compSame([2, 4, 6, 5], [4, 16, 36, 25]);
+const res = compSame([2, 4, 6, 5], [4, 16, 36, 5]);
 console.log(res);
