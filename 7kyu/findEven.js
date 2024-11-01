f18b7eead3beebbb73e976f98e68cf13db664d2d
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-01 16:51:34 +0000

    clever solution

--- 7kyu/findEven.js
+++ 7kyu/findEven.js
@@ -17,6 +17,11 @@ const findEvenIndex = (arr) => {
   return -1;
 }
 
+// function findEvenIndex(arr){
+//   const sum = arr => arr.reduce((acc,cur)=> (acc+cur) ,0)
+//   return arr.findIndex((val,idx) => sum(arr.slice(0,idx)) === sum(arr.slice(idx+1)))
+// }
+
 
 const res = findEvenIndex([4, 5, 3, 4, 5])
 console.log(res)
