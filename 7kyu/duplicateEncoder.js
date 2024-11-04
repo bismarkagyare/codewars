f1fac11ec215faf1e3ac0f2362c1284ba4bee1b0
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-04 21:10:32 +0000

    optimised solution

--- 7kyu/duplicateEncoder.js
+++ 7kyu/duplicateEncoder.js
@@ -16,3 +16,12 @@ const duplicateEncode = (str) => {
 const res = duplicateEncode("recede")
 console.log(res)
 
+// function duplicateEncode(word){
+//   return word
+//     .toLowerCase()
+//     .split('')
+//     .map( function (a, i, w) {
+//       return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+//     })
+//     .join('');
+// }
