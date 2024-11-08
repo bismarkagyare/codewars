10bad381a9df71782fe5e549110a862c2bdf8d02
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-08 11:15:16 +0000

    final solution: optimised

--- 6kyu/deleteNth.js
+++ 6kyu/deleteNth.js
@@ -1,24 +1,39 @@
+// const deleteNth = (arr, n) => {
+//   const newArr = [];
+
+//   const countArr = {};
+
+//   for (const num of arr) {
+//     countArr[num] = countArr[num] || 0;
+
+//     if (countArr[num] < n) {
+//       newArr.push(num);
+//       countArr[num] += 1;
+//     }
+//   }
+
+//   console.log(countArr);
+
+//   return newArr;
+// };
+
+// const res = deleteNth([20, 37, 20, 21], 1);
+// console.log(res);
+
 const deleteNth = (arr, n) => {
-  const newArr = []
+  const countMap = new Map()
 
-  const countArr = {}
-  
-  for (const num of arr) {
-    countArr[num] = (countArr[num] || 0) + 1
-  }
+  return arr.filter((num) => {
+    const count = countMap.get(num) || 0
 
-  for (const num of arr) {
-    if (countArr[num] < n) {
-      newArr.push(num)
-      countArr[num]++
+    if (count < n) {
+      countMap.set(num, count + 1)
+      return true
     }
-  }
 
-  console.log(countArr)
-
-  return newArr;
+    return false
+  })
 }
 
-
-const res = deleteNth([20,37,20,21])
+const res = deleteNth([20, 37, 20, 21], 1);
 console.log(res);
