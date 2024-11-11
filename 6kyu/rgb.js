ac4cf0d3f0765f1da66939de05563ad5584ca86b
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-11 21:51:56 +0000

    fix: add leading zeros

--- 6kyu/rgb.js
+++ 6kyu/rgb.js
@@ -4,15 +4,15 @@ const rgb = (r, g, b) => {
   const green = Math.min(255, Math.max(0, g))
   const blue = Math.min(255, Math.max(0, b ))
 
-  const redHex = red.toString(16).toUpperCase()
-  const greenHex = green.toString(16).toUpperCase()
-  const blueHex = blue.toString(16).toUpperCase()
+  const redHex = red.toString(16).padStart(2, "0").toUpperCase()
+  const greenHex = green.toString(16).padStart(2, "0").toUpperCase()
+  const blueHex = blue.toString(16).padStart(2, "0").toUpperCase()
 
   const hexColor = [redHex, greenHex, blueHex].join("")
 
   return hexColor;
 }
 
-const res = rgb(255, 255, 50)
+const res = rgb(255, 255, 0)
 
 console.log(res);
