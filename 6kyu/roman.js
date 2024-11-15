0564381bc25ade42babbe235541317872d67d375
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-15 14:56:13 +0000

    solution final

--- 6kyu/roman.js
+++ 6kyu/roman.js
@@ -29,9 +29,34 @@ const RomanNumerals = {
   },
 
   fromRoman: function(str) {
-    return 4
+    let total = 0
+
+    const romanToIntMap = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    };
+
+    Object.entries(romanToIntMap).forEach(([key, value]) => {
+      while (str.startsWith(key)) {
+        total += value;
+        str = str.slice(key.length); // Remove the matched symbol
+      }
+    });
+    
+    return total;
   }
 }
 
 
-console.log(RomanNumerals.toRoman(20))
+console.log(RomanNumerals.fromRoman("XLVIII"))
