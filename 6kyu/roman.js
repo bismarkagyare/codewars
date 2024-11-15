74cdfbf8af2154567d0e6181f68d2e2de2ac8dfd
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-15 11:13:33 +0000

    convert integers to roman numerals

--- 6kyu/roman.js
+++ 6kyu/roman.js
@@ -1,6 +1,31 @@
 const RomanNumerals = {
   toRoman: function(num) {
-    return 'IV'
+    let resultStr = ''
+
+    const romanNumerals = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    };
+
+    Object.entries(romanNumerals).forEach(([key, value]) => {
+      while (num >= value) {
+        num -= value
+        resultStr += key
+      }
+    })
+    
+    return resultStr;
   },
 
   fromRoman: function(str) {
@@ -9,4 +34,4 @@ const RomanNumerals = {
 }
 
 
-console.log(RomanNumerals.toRoman(4))
+console.log(RomanNumerals.toRoman(20))
