b17c51eeca3902bd05e8ac52a2add320444bb3c9
Bismark Agyare <bismarkagyare54@gmail.com>
2024-11-19 16:23:57 +0000

    solve permutations problem

--- 6kyu/permutations.js
+++ 6kyu/permutations.js
@@ -1,5 +1,22 @@
 function permutations(str) {
-  return str.slice(1)
+  if (str.length <= 1) return [str]
+
+  const result = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i]
+    const remainingChars = str.slice(0, i) + str.slice(i + 1)
+
+    //recursively get permutations of remaining chars
+    const permsOfRemaining = permutations(remainingChars)
+
+    for (const perm of permsOfRemaining) {
+      result.add(currentChar + perm)
+    }
+  }
+
+  return Array.from(result)
 }
 
-console.log(permutations("abc"));
+const res = permutations("abc")
+console.log(res)
